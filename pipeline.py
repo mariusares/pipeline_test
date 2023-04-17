5b73ca6089d94fa82a25bf5efe81c67cd0c932a8
mariusares <63591661+mariusares@users.noreply.github.com>
2023-04-17 16:47:35 +0100

    Add files via upload

---
+++ pipeline.py
@@ -0,0 +1,12 @@
+import os
+List1 = [4, 7, 9, 5, 2]
+List1.reverse()
+print ("The reversed list is", List1)
+List2 = [3, 1, 8, 6]
+List2.sort()
+print ("The sorted list is", List2)
+List3 = []
+List3 = List1.copy()
+print("List3 = ", List3)
+indexvalue=List2[2:4]
+print ("The index values are ", indexvalue)
